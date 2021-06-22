517b362b0543e9fa19642b65d9c1152efe00b35e
serverless-user-at-583044482327 <desaijay315@gmail.com>
2021-06-22 15:34:45 +0530

    mocha report

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -16,7 +16,14 @@
  * @type {Cypress.PluginConfig}
  */
 // eslint-disable-next-line no-unused-vars
+
+const cucumber = require('cypress-cucumber-preprocessor').default
+// This function is called when a project is opened or re-opened (e.g. due to
+// the project's config changing)
+
 module.exports = (on, config) => {
+  on('file:preprocessor', cucumber())
+
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
 }
